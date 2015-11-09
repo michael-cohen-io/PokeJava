5ba3d5590fe4d2b39eb6e1817caaf6fdf16e93ff
peternerd <p.c.q1109@gmail.com>
2015-11-08 18:55:23 -0600

    Added the getPokemonsNames()

--- src/com/pokejava/Pokedex.java
+++ src/com/pokejava/Pokedex.java
@@ -1,6 +1,9 @@
 package com.pokejava;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.TreeMap;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -11,8 +14,8 @@ import org.json.JSONObject;
  */
 public class Pokedex extends ModelClass {
 	
-
-	private ArrayList<Integer> Pokemons;
+	//I thing it will be better to have a Map with the PokemonId and the name
+	private Map<Integer,String> Pokemons;
 	
 	public Pokedex(){
 		String data = "";
@@ -30,13 +33,17 @@ public class Pokedex extends ModelClass {
 			
 			//Pokemon ArrayList defining
 			JSONArray pokemonNode = root.getJSONArray("pokemon");
-			Pokemons = new ArrayList<Integer>();
+			//I used a TreeMap because it will be sorted by keys
+			Pokemons = new TreeMap<Integer,String>();
+			
 				for (int i = 0; i < pokemonNode.length(); i++) {
 					String pokemonURI = pokemonNode.getJSONObject(i).getString("resource_uri");
+					//gets the pokemon name
+					String pokemonName = pokemonNode.getJSONObject(i).getString("name");
 					pokemonURI = pokemonURI.substring(15);
 					pokemonURI = pokemonURI.replace("/", "");
-					
-					Pokemons.add(Integer.parseInt(pokemonURI));
+					//puts the pokemonId and the name into the map
+					Pokemons.put(Integer.parseInt(pokemonURI),pokemonName);
 				}
 			
 		} catch (Exception e) {
@@ -60,5 +67,14 @@ public class Pokedex extends ModelClass {
 	public boolean hasPokemon(){
 		if (Pokemons.isEmpty()) return false;
 		else return true;
+	}
+	
+	/**
+	 * I need a method that return all the pokemons names
+	 * If I use the getPokemons() method it will do one request per Pokemon so it will be slow to get all pokemons
+	 * @return A collection with all the pokemons names
+	 */
+	public Collection<String> getPokemonsNames(){
+		return Pokemons.values();
 	}
 }
